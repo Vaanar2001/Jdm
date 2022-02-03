1cf2cc67ee4e4486ee7257246f4a6fe063cab8b4
vasu <vasugarg4850@gmail.com>
2022-02-03 09:05:52 -0800

    Navbar problem solved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home.jsx';
 import About from './About.jsx';
